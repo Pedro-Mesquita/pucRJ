fdd48a41ed9dc2a68b5d72fe0b75fd544c1151ff
Pedro Mesquita <44656009+Pedro-Mesquita@users.noreply.github.com>
2023-03-20 17:28:06 -0300

    1 pdf de exercícios

---
+++ trabalho ctc 4002.py
@@ -0,0 +1,71 @@
+#1
+print(3**2 )
+print(3**2.0)
+print(-3**2)
+print((-3)** 2)
+print(27**(1/3))
+print(25**0.5)
+print(25**(1/2))
+print(25**1/2 )
+print("====================")
+#2
+print(9/6)
+print(9.0/6)
+print(9//6)
+print(-9/6.0)
+print(-9/6)
+print(-9//6)
+print("====================")
+#3
+print(9%6)
+print(9.0%6)
+print(9%5)
+print("====================")
+#4
+print(int(12.7))
+print(float(12))
+print("====================")
+#5
+print(7==7)
+#print(7=7) error
+print(7>4)
+print(7<4 )
+print(8!=2)
+print(8!=8)
+print(8>=4)
+print(8<=8)
+print("====================")
+#6
+print('Oi' + ', tudo bem?')
+print('Eu' * 10)
+print(3 * 'Também' + ' gosto' )
+#print('Pode dividir?' / 13 ) error
+print(len('Maya'))
+print(len('Eu' * 10))
+print('m' in 'Maya' )
+print('y' in 'Maya' )
+# print('Eu' + 10 ) error
+print('Eu' + str(10))
+print('12'+'24')
+print(int('12')+int('24'))
+print(float('12.5')+float('24'))
+#print(float('ju')) error
+#print(int('ju')) error
+#print(int('12.5')) error
+#7
+print("====================")
+print(['seg','ter','qua'] + [2,3,4])
+print([2,3] * 10 )
+print(3 * ['oi','oi'] + ['té'])
+#print([1,2,3,4,5,6]/ 2) error
+print(len([12,'Maya',4/2]))
+#print([1,2,3]+'um dois três') error
+print( [1,2,3]+ list('um dois três'))
+print(str([1,2,3])+'um dois três' )
+
+
+
+
+
+
+
