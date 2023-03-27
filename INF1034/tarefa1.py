0f84b7842dcddb80a8ff39311232866b20335766
pedro mesquita <pedro.mesquitamaia@gmail.com>
2023-03-27 19:30:33 -0300

    bandeira de Gana

--- INF1034/tarefa1.py
+++ INF1034/tarefa1.py
@@ -1,8 +1,8 @@
 # 1) Desenhar o plano cartesiano (eixos x e y) (opcional, não incluso na nota: com a seta ao final de cada um e numerados);
 # 2) Desenhar uma forma geométrica, pelo menos, em cada quadrante (usar repetição e fazer pelo menos duas diferentes da sala de aula, com cores distintas para cada);
 # 3) Desenhar uma espiral (sentido horário ou anti-horario) em algum dos quadrantes;
-
 # 4) Desenhar uma bandeira fácil (Marrocos, ou uma com 2 a 3 formas geométricas, não necessariamente diferentes);
+
 # 5) Desenhar uma bandeira média (Turquia, ou uma com 3 a 4 formas geométricas, não necessariamente diferentes);
 # 6) Desenhar uma bandeira difícil (África do Sul, ou uma com mais de 4 formas geométricas, não necessariamente diferentes);
 # DESAFIOS (opcional, não incluso na nota)
@@ -10,6 +10,7 @@
 # - Implementar o código usando uma função para cada elemento diferente (bandeira, plano, forma geométrica, etc.)
 
 import turtle
+import math
 
 #definções básicas
 t = turtle.Turtle()
@@ -17,15 +18,6 @@ t.color("black")
 t.screen.setup(800, 800)
 t.screen.screensize(800, 800)
 t.speed(100000000)
-
-def bandeira():
-    t.fd(150)
-    t.left(90)
-    t.fd(100)
-    t.left(90)
-    t.fd(150)
-    t.left(90)
-    t.fd(100)
 
 def triangulo(t):
     t.begin_fill()
@@ -124,32 +116,64 @@ for x in range(50):
 t.up()
 t.home()
 posicao(t,-200,200)
-bandeira()
 t.color("red")
-t.begin_fill()
-t.left(180)
-t.fd(100/3)
-t.right(90)
-t.fd(150)
-t.right(90)
-t.fd(100/3)
-t.end_fill()
+
+def bandeira_litras(t,cor_um, cor_dois, cor_tres):
+    t.color(cor_um)
+    t.begin_fill()
+    t.left(180)
+    t.fd(100/3)
+    t.right(90)
+    t.fd(150)
+    t.right(90)
+    t.fd(100/3)
+    t.end_fill()
+    
+    t.left(180)
+    t.fd(100/3)
+    t.color(cor_dois)
+    t.begin_fill()
+    t.left(90)
+    t.fd(150)
+    t.right(90)
+    t.fd(100/3)
+    t.right(90)
+    t.fd(150)
+    t.end_fill()
+    
+    t.color(cor_tres)
+    t.begin_fill()
+    t.left(90)
+    t.fd(100/3)
+    t.left(90)
+    t.fd(150)
+    t.left(90)
+    t.fd(100/3)
+    t.end_fill()
+
 t.up()
-t.left(180)
-t.fd(2*(100/3))
+t.home()
 t.down()
+t.left(90)
+posicao(t, -200, 200)
+bandeira_litras(t, "red", "white", "red")
+
+t.up()
+t.home()
+t.down()
+posicao(t,-200,-200)
+t.left(90)
+bandeira_litras(t, "green", "yellow", "red")
+t.left(90)
+t.fd(150/1.8)
+t.right(105)
+
 t.begin_fill()
-t.left(90)
-t.fd(150)
-t.right(90)
-t.fd(100/3)
-t.right(90)
-t.fd(150)
-t.right(90)
-t.fd(100/3)
+t.down()
+for i in range(5):
+    t.fd(35)
+    t.right(144)
+
 t.end_fill()
-posicao(t,-200,-250)
-t.left(90)
-bandeira()
 
 t.getscreen()._root.mainloop()
