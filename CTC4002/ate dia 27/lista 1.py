4fc08559c7c586f7109524691ca474f18553c3ad
pedro mesquita <pedro.mesquitamaia@gmail.com>
2023-03-27 21:18:37 -0300

    feito do 1 ao 5

---
+++ CTC4002/ate dia 27/lista 1.py
@@ -0,0 +1,25 @@
+#exercício 1
+# lado_triangulo = float(input("Lado do triângulo: "))
+# area_triangulo = ((3**0.5)/4)*lado_triangulo**2
+# print('Á área do triângulo é %.2f cm²'%(area_triangulo))
+
+#exercício 2
+# base_maior = float(input("Digite em cm o tamanho da base maior: "))
+# base_menor = float(input("Digite em cm o tamanho da base menor: "))
+# altura = float(input("Digite em cm o tamanho da altura: "))
+# area_trapezio = ((base_maior+base_menor)*altura)/2
+
+#exercício 4
+# per_votos_nulos_brancos = 100-26-24-22
+# total_votos = 196/(per_votos_nulos_brancos/100)
+# canditato_vencedor = (26*total_votos)/100
+# print('Percentual de votos nulos e brancos: %d\nQuantidade de votos do candidato A: %d'%(per_votos_nulos_brancos, canditato_vencedor))
+
+#exercício 5
+# altura_cm = int(input("Altura em cm: "))
+# if altura_cm//100 <=0:
+#     print("A sua altura é de 0m e %dcm"%(altura_cm))
+# else:
+#     print("A sua altura é de %dm e %dcm"%(altura_cm//100, altura_cm%100))
+
+#exercício 6
