434edea41ba82451a4e0f97ec8b41350b91925cb
Pedro Mesquita <44656009+Pedro-Mesquita@users.noreply.github.com>
2023-05-04 10:52:25 -0300

    top

--- INF1034/cripto.py
+++ INF1034/cripto.py
@@ -1,32 +1,36 @@
+#ex 1
+
 def validar_cpf(cpf):
     cpf_modificado = cpf.replace('-', '')
     cpf_modificado = cpf_modificado.replace('.', '')
     lista_cpf = list(cpf_modificado)
-    lista_cpf_modificada = null
-    i = 0
-    x = 0
-    acumulador_um = 0
-    acumulador_dois = 0
+    lista_top = lista_cpf[0:9]
+    acumulador_um = acumulador_dois = primerio_digito_verificador = segundo_digito_verificador = i = x = 0
     multiplicador = 10
     multiplicador_um = 11
-    primerio_digito_verificador = 0
     
     while i < 9:
         acumulador_um = (multiplicador - i) * int(lista_cpf[i]) + acumulador_um
         i = i + 1
+        
     if acumulador_um % 11 >= 2:
         primerio_digito_verificador = 11 - acumulador_um % 11
         
+    lista_top.append(str(primerio_digito_verificador))
     
-    while x <= 10:
-        acumulador_dois = (multiplicador_um - x) * int(lista_cpf[x]) + acumulador_dois
+    while x < 10:
+        acumulador_dois = (multiplicador_um - x) * int(lista_top[x]) + acumulador_dois
         x = x + 1
-    print(lista_cpf)
-    print(acumulador_um)
-    print(acumulador_dois)
-        
     
+    if acumulador_dois % 11 >= 2:
+        segundo_digito_verificador = 11 - acumulador_dois % 11
+    lista_top.append(str(segundo_digito_verificador))
+  
+    if str(primerio_digito_verificador) == lista_cpf[9] and str(segundo_digito_verificador) == lista_cpf[10]:
+        return True
+    else:
+        return False
+    
+print(validar_cpf('111.444.777-05'))
         
-
-validar_cpf('111.444.777-05')
-        
+#ex 2
