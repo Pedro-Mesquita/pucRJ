c44c8b5e5c83135d4985b572965e43131ea576d5
Pedro-Mesquita <pedro.mesquitamaia@gmail.com>
2023-05-10 22:03:12 -0300

    calculadora terminada

--- INF1034/calc.py
+++ INF1034/calc.py
@@ -2,8 +2,8 @@
 
 import pygame
 
-width = 800  #Largura Janela
-height = 600 #Altura Janela
+width = 440  #Largura Janela
+height = 500 #Altura Janela
 
 WHITE = [255, 218, 166]
 global result
@@ -20,159 +20,153 @@ def draw_screen(screen):
     screen.fill(WHITE)
 
     #Cria janela de resultado
-    pygame.draw.rect(screen, (255,177,69    ), (50, 100, 700, 100))
+    pygame.draw.rect(screen, (255,177,69), (50, 50, 340, 100))
     t = sys_font.render(result, False, WHITE)
-    screen.blit(t, (85,125))
+    screen.blit(t, (85,85))
     
     #Cria Botão 1 (contém numero 7)
-    pygame.draw.rect(screen, (116, 171, 144), (50, 250, 60, 60))
+    pygame.draw.rect(screen, (116, 171, 144), (50, 200, 60, 60))
     t = sys_font.render("7", False, WHITE)
-    screen.blit(t, (65,265))
+    screen.blit(t, (65,215))
     
     #Cria Botão 2 (contém numero 8)
-    pygame.draw.rect(screen, (116, 171, 144), (120, 250, 60, 60))
+    pygame.draw.rect(screen, (116, 171, 144), (120, 200, 60, 60))
     t = sys_font.render("8", False, WHITE)
-    screen.blit(t, (135,265))
+    screen.blit(t, (135,215))
     
     #Cria Botão 3 (contém numero 9)
-    pygame.draw.rect(screen, (116, 171, 144), (190, 250, 60, 60))
+    pygame.draw.rect(screen, (116, 171, 144), (190, 200, 60, 60))
     t = sys_font.render("9", False, WHITE)
-    screen.blit(t, (205,265))
+    screen.blit(t, (205,215))
 
     #Cria Botão 4 (contém numero 4)
-    pygame.draw.rect(screen, (116, 171, 144), (50, 320, 60, 60))
+    pygame.draw.rect(screen, (116, 171, 144), (50, 270, 60, 60))
     t = sys_font.render("4", False, WHITE)
-    screen.blit(t, (65,335))
+    screen.blit(t, (65,285))
     
     #Cria Botão 5 (contém numero 5)
-    pygame.draw.rect(screen, (116, 171, 144), (120, 320, 60, 60))
+    pygame.draw.rect(screen, (116, 171, 144), (120, 270, 60, 60))
     t = sys_font.render("5", False, WHITE)
-    screen.blit(t, (135,335))
+    screen.blit(t, (135,285))
 
     #Cria Botão 6 (contém numero 6)
-    pygame.draw.rect(screen, (116, 171, 144), (190, 320, 60, 60))
+    pygame.draw.rect(screen, (116, 171, 144), (190, 270, 60, 60))
     t = sys_font.render("6", False, WHITE)
-    screen.blit(t, (205,335))
+    screen.blit(t, (205,285))
 
     #Cria Botão 7 (contém numero 1)
-    pygame.draw.rect(screen, (116, 171, 144), (50, 390, 60, 60))
+    pygame.draw.rect(screen, (116, 171, 144), (50, 340, 60, 60))
     t = sys_font.render("1", False, WHITE)
-    screen.blit(t, (65,405))
+    screen.blit(t, (65,355))
     
     #Cria Botão 8 (contém numero 2)
-    pygame.draw.rect(screen, (116, 171, 144), (120, 390, 60, 60))
+    pygame.draw.rect(screen, (116, 171, 144), (120, 340, 60, 60))
     t = sys_font.render("2", False, WHITE)
-    screen.blit(t, (135,405))
+    screen.blit(t, (135,355))
 
     #Cria Botão 9 (contém numero 3)
-    pygame.draw.rect(screen, (116, 171, 144), (190, 390, 60, 60))
+    pygame.draw.rect(screen, (116, 171, 144), (190, 340, 60, 60))
     t = sys_font.render("3", False, WHITE)
-    screen.blit(t, (205,405))
+    screen.blit(t, (205,355))
 
     #Cria Botão 10 (contém x)
-    pygame.draw.rect(screen, (255,177,69), (50, 460, 60, 60))
+    pygame.draw.rect(screen, (255,177,69), (50, 410, 60, 60))
     t = sys_font.render("C", False, WHITE)
-    screen.blit(t, (65,475))
+    screen.blit(t, (65,425))
 
     #Cria Botão 11 (contém numero 3)
-    pygame.draw.rect(screen, (116, 171, 144), (120, 460, 60, 60))
+    pygame.draw.rect(screen, (116, 171, 144), (120, 410, 60, 60))
     t = sys_font.render("0", False, WHITE)
-    screen.blit(t, (135,475))
+    screen.blit(t, (135,425))
     
     #Cria Botão 12 (contém numero 3)
-    pygame.draw.rect(screen, (255,177,69), (190, 460, 60, 60))
+    pygame.draw.rect(screen, (255,177,69), (190, 410, 60, 60))
     t = sys_font.render(".", False, WHITE)
-    screen.blit(t, (205,475))
-    
-    
+    screen.blit(t, (205,425))
 
     #Cria Botão 3 (contém operador *)
-    pygame.draw.rect(screen, (255,177,69), (260, 250, 60, 60))
+    pygame.draw.rect(screen, (255,177,69), (260, 200, 60, 60))
     t = sys_font.render("+", False, WHITE)
-    screen.blit(t, (275,265))
+    screen.blit(t, (275,215))
 
     #Cria Botão 3 (contém operador +)
-    pygame.draw.rect(screen, (255,177,69), (330, 250, 60, 60))
+    pygame.draw.rect(screen, (255,177,69), (330, 200, 60, 60))
     t = sys_font.render("-", False, WHITE)
-    screen.blit(t, (345,265))
+    screen.blit(t, (345,215))
 
-    pygame.draw.rect(screen, (255,177,69), (260, 320, 60, 60))
+    pygame.draw.rect(screen, (255,177,69), (260, 270, 60, 60))
     t = sys_font.render("*", False, WHITE)
-    screen.blit(t, (275,335))
+    screen.blit(t, (275,285))
 
     #Cria Botão 3 (contém operador +)
-    pygame.draw.rect(screen, (255,177,69), (330, 320, 60, 60))
+    pygame.draw.rect(screen, (255,177,69), (330, 270, 60, 60))
     t = sys_font.render("/", False, WHITE)
-    screen.blit(t, (345,335))
+    screen.blit(t, (345,285))
 
-    pygame.draw.rect(screen, (255,177,69), (260, 390, 60, 60))
+    pygame.draw.rect(screen, (255,177,69), (260, 340, 60, 60))
     t = sys_font.render("//", False, WHITE)
-    screen.blit(t, (275,405))
+    screen.blit(t, (275,355))
 
     #Cria Botão 3 (contém operador +)
-    pygame.draw.rect(screen, (255,177,69), (330, 390, 60, 60))
+    pygame.draw.rect(screen, (255,177,69), (330, 340, 60, 60))
     t = sys_font.render("%", False, WHITE)
-    screen.blit(t, (345,405))
+    screen.blit(t, (345,355))
 
-    pygame.draw.rect(screen, (255,177,69), (260, 460, 60, 60))
+    pygame.draw.rect(screen, (255,177,69), (260, 410, 60, 60))
     t = sys_font.render("**", False, WHITE)
-    screen.blit(t, (275,475))
+    screen.blit(t, (275,425))
 
     #Cria Botão 3 (contém operador +)
-    pygame.draw.rect(screen, (116, 171, 144), (330, 460, 60, 60))
+    pygame.draw.rect(screen, (116, 171, 144), (330, 410, 60, 60))
     t = sys_font.render("=", False, WHITE)
-    screen.blit(t, (345,475))
-
-
-
-    pass
+    screen.blit(t, (345,425))
 
 def mouse_click_down(px_mouse, py_mouse, mouse_buttons):
     global result
     if mouse_buttons[0]:
-        if check_click(50, 250, 60, 60, px_mouse, py_mouse):
+        if check_click(50, 200, 60, 60, px_mouse, py_mouse):
             result = result + "7"
-        if check_click(120, 250, 60, 60, px_mouse, py_mouse):
+        if check_click(120, 200, 60, 60, px_mouse, py_mouse):
             result = result + "8"
-        if check_click(190, 250, 60, 60, px_mouse, py_mouse):
+        if check_click(190, 200, 60, 60, px_mouse, py_mouse):
             result = result + "9"
-        if check_click(50, 320, 60, 60, px_mouse, py_mouse):
+        if check_click(50, 270, 60, 60, px_mouse, py_mouse):
             result = result + "4"
-        if check_click(120, 320, 60, 60, px_mouse, py_mouse):
+        if check_click(120, 270, 60, 60, px_mouse, py_mouse):
             result = result + "5"
-        if check_click(190, 320, 60, 60, px_mouse, py_mouse):
+        if check_click(190, 270, 60, 60, px_mouse, py_mouse):
             result = result + "6"
-        if check_click(50, 390, 60, 60, px_mouse, py_mouse):
+        if check_click(50, 340, 60, 60, px_mouse, py_mouse):
             result = result + "1"
-        if check_click(120, 390, 60, 60, px_mouse, py_mouse):
+        if check_click(120, 340, 60, 60, px_mouse, py_mouse):
             result = result + "2"
-        if check_click(190, 390, 60, 60, px_mouse, py_mouse):
+        if check_click(190, 340, 60, 60, px_mouse, py_mouse):
             result = result + "3"
-        if check_click(120, 460, 60, 60, px_mouse, py_mouse):
+        if check_click(120, 410, 60, 60, px_mouse, py_mouse):
             result = result + "0"
-        if check_click(260, 320, 60, 60, px_mouse, py_mouse):
+        if check_click(260, 270, 60, 60, px_mouse, py_mouse):
             result = result + "*"  
-        if check_click(260, 250, 60, 60, px_mouse, py_mouse):
+        if check_click(260, 200, 60, 60, px_mouse, py_mouse):
             result = result + "+"   
-        if check_click(330, 250, 60, 60, px_mouse, py_mouse):
+        if check_click(330, 200, 60, 60, px_mouse, py_mouse):
             result = result + "-"   
-        if check_click(330, 320, 60, 60, px_mouse, py_mouse):
+        if check_click(330, 270, 60, 60, px_mouse, py_mouse):
             result = result + "/"   
-        if check_click(260, 390, 60, 60, px_mouse, py_mouse):
+        if check_click(260, 340, 60, 60, px_mouse, py_mouse):
             result = result + "//"   
-        if check_click(330, 390, 60, 60, px_mouse, py_mouse):
+        if check_click(330, 340, 60, 60, px_mouse, py_mouse):
             result = result + "%"   
-        if check_click(260, 460, 60, 60, px_mouse, py_mouse):
+        if check_click(260, 410, 60, 60, px_mouse, py_mouse):
             result = result + "**"   
-
-        if check_click(190, 460, 60, 60, px_mouse, py_mouse):
+        if check_click(190, 410, 60, 60, px_mouse, py_mouse):
             result = result + "."   
-        if check_click(50, 460, 60, 60, px_mouse, py_mouse):
+        if check_click(50, 410, 60, 60, px_mouse, py_mouse):
             result = result[0:len(result)-1]   
-
-
-        if check_click(330, 460, 60, 60, px_mouse, py_mouse):
-            result = str(eval(result))
+        if check_click(330, 410, 60, 60, px_mouse, py_mouse):
+            try:
+                result = str(round(eval(result), 2))
+            except:
+                result = 'ERROR'
                
 def main_loop(screen):
     running = True
