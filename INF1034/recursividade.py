37d00cd51af42ce4870457a91e09f6040f06e3a7
Pedro Mesquita <44656009+Pedro-Mesquita@users.noreply.github.com>
2023-05-16 10:57:03 -0300

    recursividade

--- INF1034/recursividade.py
+++ INF1034/recursividade.py
@@ -1,16 +1,16 @@
-# def potencia(x, y):
-#     if y == 0:
-#         return 1
-#     return x * potencia(x, y-1)
+def potencia(x, y):
+    if y == 0:
+        return 1
+    return x * potencia(x, y-1)
 
-# print(potencia(3,3))
+print(potencia(3,3))
         
-# def fat(n):
-#     if n == 0:
-#         return 1
-#     return n * fat(n - 1)
+def fat(n):
+    if n == 0:
+        return 1
+    return n * fat(n - 1)
 
-# print(fat(5))
+print(fat(5))
 
 def fibo(x):
     if x == 1:
@@ -19,4 +19,20 @@ def fibo(x):
         return 0
     return fibo(x-1) + fibo(x-2)
 
-print(fibo(5))
+print(fibo(6))
+
+def somaAlg(numero):
+    if numero < 10:
+        return numero
+    return numero % 10 + somaAlg(numero // 10)
+
+def mostra_num(num):
+    if num < 10:
+        print(num)
+        return num
+    numAnt = num//10
+    u = num % 10
+    mostra_num(numAnt)
+    print(u)
+    
+mostra_num(230)
