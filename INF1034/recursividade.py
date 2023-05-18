b7988d2edf0891d3f3b38f9a48749fff7e5a8342
Pedro Mesquita <44656009+Pedro-Mesquita@users.noreply.github.com>
2023-05-18 10:03:38 -0300

    adicionado mais funções recursivas

--- INF1034/recursividade.py
+++ INF1034/recursividade.py
@@ -36,3 +36,30 @@ def mostra_num(num):
     print(u)
     
 mostra_num(230)
+
+def mostra_num(num):
+    if num < 10:
+        return 1
+    return 1 + mostra_num(num // 10)
+
+print(mostra_num(2234))
+
+def pares(num):
+    if num < 10:
+        if num % 2 == 0:
+            return 1
+        return 0
+    if num % 2 == 0:
+        return 1 + pares(num // 10)
+    return pares(num // 10)
+
+
+print(pares(123456821))
+
+def bomDia(n):
+    if n == 0:
+        return
+    print("Bom dia")
+    return bomDia(n - 1)
+
+bomDia(10)
