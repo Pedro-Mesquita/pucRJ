2e7aa51d08d93cb3a9b44d2b41dcca77ce508f75
Pedro-Mesquita <pedro.mesquitamaia@gmail.com>
2023-05-22 22:11:03 -0300

    feitos alguns exerícios de recursão

--- CTC4002/funções_recursivas.py
+++ CTC4002/funções_recursivas.py
@@ -1,3 +1,84 @@
+# lista 1
+
+def mul_int(int1, int2):
+    if int1 == 0 or int2 == 0:
+        return 0
+    return int1 + (int1 * mul_int(int1, int2-1))
+
+def len_num(inteiro):
+    if inteiro < 10:
+        return 1
+    return 1 + len_num(inteiro//10)
+
+def qtd_3_ou_4(num):
+    if num < 3:
+        return 0
+    if num % 10 == 3 or num % 10 == 4:
+        return 1 + qtd_3_ou_4(num//10)
+    return qtd_3_ou_4(num//10)
+
+# def print_por_linha(num):
+#     if num < 10:
+#         print(num)
+#         return 
+#     print(num // 10)
+#     return print_por_linha(num % 10)
+
+def print_por_linha(num):
+    if num < 10:
+        print(num)
+        return 
+    print(num % 10)
+    return print_por_linha(num // 10)
+
+def fat(num):
+    if num == 0:
+        return 1
+    return num * fat(num-1)
+
+def len_str(s):
+    if s == '':
+        return 0
+    return 1 + len_str(s[1:])
+
+def find_bool(c, s):
+    if s == '':
+        return False
+    if s[0] == c:
+        return True
+    return find_bool(c, s[1:])
+
+def print_por_linha(s):
+    if s == '':
+        print('')
+    if len(s) == 1:
+        print(s)
+        return
+    print(s[0])
+    return print_por_linha(s[1:])
+
+# def print_por_linha_invertido(s):
+#     if len(s) == 0:
+#         return ' '
+#     if len(s) == 1:
+#         print(s)
+#         return
+#     print(s[-1])
+#     return print_por_linha_invertido(s[:-1])
+
+
+def find(c, s):
+    if s == '':
+        return 0
+    if s[0] == c:
+        return 1 + find(c, s[1:])
+    return find(c, s[1:])
+
+
+
+
+
+# lista 2
 def conta34(num):
     if num < 34:
         return 0
