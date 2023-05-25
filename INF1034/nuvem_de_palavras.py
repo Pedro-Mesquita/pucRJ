5536fa2d1bc258e51d996bfd9fa42cd1c4ac25c4
Pedro-Mesquita <pedro.mesquitamaia@gmail.com>
2023-05-25 06:34:27 -0300

    mudanças

--- INF1034/nuvem_de_palavras.py
+++ INF1034/nuvem_de_palavras.py
@@ -1,7 +1,6 @@
-import turtle
-import eixos_turtle
+# import turtle
+# import eixos_turtle
 
-letras = string.ascii_uppercase
 
 texto = 'Este samba Vai pra Dorival Caymmi João Gilberto e Caetano Veloso Vamo lá  O Rio de Janeiro continua lindo O Rio de Janeiro continua sendo O Rio de Janeiro fevereiro e março  Alô, alô Realengo aquele abraço Alô torcida do Flamengo aquele abraço Alô alô Realengo aquele abraço Alô torcida do Flamengo aquele abraço  Chacrinha continua balançando a pança E buzinando a moça e comandando a massa E continua dando as ordens no terreiro  Alô alô seu Chacrinha velho guerreiro Alô alô Terezinha Rio de Janeiro Alô alô seu Chacrinha velho palhaço Alô alô Terezinha aquele abraço  Alô moça da favela aquele abraço Todo mundo da Portela aquele abraço Todo mês de fevereiro aquele passo Alô Banda de Ipanema aquele abraço  Meu caminho pelo mundo eu mesmo traço A Bahia já me deu régua e compasso Quem sabe de mim sou eu Aquele abraço Pra você que me esqueceu Aquele abraço  Alô Rio de Janeiro aquele abraço Todo o povo brasileiro aquele abraço  O Rio de Janeiro continua lindo O Rio de Janeiro continua sendo O Rio de Janeiro, fevereiro e março  Alô alô Realengo Alô torcida do Flamengo aquele abraço Alô alô Realengo aquele abraço Alô torcida do Flamengo aquele abraço  Chacrinha continua balançando a pança E buzinando a moça e comandando a massa E continua dando as ordens no terreiro  Alô alô seu Chacrinha velho guerreiro Alô alô Terezinha Alô alô seu Chacrinha Alô alô Terezinha aquele abraço  Alô moça da favela aquele abraço Todo mundo da Portela aquele abraço Todo mês de fevereiro aquele passo Alô Banda de Ipanema aquele abraço  Meu caminho pelo mundo eu mesmo traço A Bahia já me deu graças a Deus régua e compasso Quem sabe de mim sou eu é claro aquele abraço Pra você que me esqueceu Aquele abraço  Alô Rio de Janeiro todo mundo aquele abraço Todo o povo brasileiro aquele abraço Todo mês de fevereiro aquele abraço Alô moça da favela aquele abraço Todo mundo da Portela e do Salgueiro e todo o Rio de Janeiro aquele abraço E todo mês de fevereiro e todo povo brasileiro aquele abraço  Alô minha nega-samba aquele abraço E alô alô da pipoca aquele abraço E todo Rio de Janeiro aquele abraço E todo povo brasileiro todo povo brasileiro Todo povo brasileiro, todo povo brasileiro Todo povo brasileiro (aquele abraço!)  (Aquele abraço!) (Aquele abraço!) (Aquele abraço!) (Aquele abraço!)'
 texto = texto.upper()
@@ -22,7 +21,7 @@ for palavra in lista_filtrada_sem_repeticao:
     
 print(sorted(lista_palavras_e_incidencia, key=lambda x: x[1], reverse=True))
 
-t = turtle.Turtle()
-eixos_turtle.plotEixosCartesianos(t,25,15)
+# t = turtle.Turtle()
+# eixos_turtle.plotEixosCartesianos(t,25,15)
 
-for x in range(0,200, 20)
+# for x in range(0,200, 20)
