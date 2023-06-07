20d711b7d8e40e92cb193cc3ab68dc9c17aca7ae
Pedro Mesquita <44656009+Pedro-Mesquita@users.noreply.github.com>
2023-06-07 11:53:02 -0300

    Update fractais.py

--- INF1034/fractais.py
+++ INF1034/fractais.py
@@ -1,20 +1,43 @@
 import turtle
+
 t = turtle.Turtle()
+t.speed(0)
+t.up()
+t.goto(-100,-100)
+t.down()
 
-def squares(t, line):
-    for x in range(5):
+def quadrados(t, line):
+    for i in range(4):
         t.fd(line)
-        t.left(90)
-    squares(line/2)
+        t.lt(90)
 
-# squares(100)
-def star(t, size):
-    if size <= 10:
+    line  =  (line - 20)/2
+    if line < 20:
         return
+    for i in range(4):
+        quadrados(t, line)
+        t.fd(2*line+20)
+        t.lt(90)
+
+# quadrados(t, 300)
+
+# def star(t, size):
+#     if size <= 10:
+#         return
+#     for i in range(5):
+#         t.fd(size)
+#         star(t, size/2)
+#         t.left(216)
+
+def star(t, size, i):
+    if i == 3:
+        if size <= 10:
+            return
     for i in range(5):
         t.fd(size)
         star(t, size/2)
         t.left(216)
+    
 
 
 star(t, 320)
