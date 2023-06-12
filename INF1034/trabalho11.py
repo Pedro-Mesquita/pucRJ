ae69c1d096f62490914b57942b80185e57c9e65b
Pedro Mesquita <44656009+Pedro-Mesquita@users.noreply.github.com>
2023-06-12 12:04:52 -0300

    Update trabalho11.py

--- INF1034/trabalho11.py
+++ INF1034/trabalho11.py
@@ -5,10 +5,14 @@ centena = ['cem', 'duzentos', 'trezentos', 'quatrocentos', 'quinhentos', 'seisce
 
 def transforma_extenso(numero):
     if numero < 10:
-        print(ate9[numero-1])
-        return
+        if numero != 0:
+            print(ate9[numero-1])
+            return 
     if numero < 20:
         print(ate19[numero%10])
+        return 
+    if numero == 100:
+        print('cem')
         return 
     if numero < 100:
         print(dezenas[(numero//10)-2])
@@ -16,5 +20,4 @@ def transforma_extenso(numero):
     if numero < 1000:
         print(centena[(numero//100)-1])
         return transforma_extenso(numero%10)
-transforma_extenso(100)
-
+transforma_extenso(200)
