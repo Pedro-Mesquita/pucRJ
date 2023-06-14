d1f7fc6435092cc6680ad69d35d411efe36dbc5e
Pedro Mesquita <44656009+Pedro-Mesquita@users.noreply.github.com>
2023-06-14 11:48:08 -0300

    Update trabalho11.py

--- INF1034/trabalho11.py
+++ INF1034/trabalho11.py
@@ -1,23 +1,33 @@
 ate9 = ['um', 'dois','tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove']
-ate19 = ['dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenovo']
+ate19 = ['dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove']
 dezenas = ['vinte', 'trinta', 'quarenta', 'cinquenta', 'sessenta', 'setenta', 'oitenta','noventa']
-centena = ['cem', 'duzentos', 'trezentos', 'quatrocentos', 'quinhentos', 'seiscentos','setecentos','oitocentos','novecentos']
+centena = ['cento', 'duzentos', 'trezentos', 'quatrocentos', 'quinhentos', 'seiscentos','setecentos','oitocentos','novecentos']
 
 def transforma_extenso(numero):
     if numero < 10:
         if numero != 0:
-            print(ate9[numero-1])
-            return 
+            return ate9[numero-1]
+        return ''
     if numero < 20:
-        print(ate19[numero%10])
-        return 
+        return ate19[numero%10]
     if numero == 100:
-        print('cem')
-        return 
+        return 'cem'
     if numero < 100:
-        print(dezenas[(numero//10)-2])
-        return transforma_extenso(numero%10)
+        if numero%10 !=0:
+            return dezenas[(numero//10)-2] + ' e ' + transforma_extenso(numero%10)
+        return dezenas[(numero//10)-2]
     if numero < 1000:
-        print(centena[(numero//100)-1])
-        return transforma_extenso(numero%10)
-transforma_extenso(200)
+        if numero//10%10 != 0:
+            return centena[(numero//100)-1] +' e '+ transforma_extenso(numero%100)
+        return  centena[(numero//100)-1] +' e '+ transforma_extenso(numero%10)
+    
+switch = True
+
+while switch:
+    numero = int(input("Entre com um númunero inteiro de 1 a 999: "))
+    print('Número em extenso: %s'%(transforma_extenso(numero)))
+    resposta = input("Deseja pedir um novo valor por extenso? (sim/não)")
+    if resposta != 'sim':
+        switch = False
+        
+    
