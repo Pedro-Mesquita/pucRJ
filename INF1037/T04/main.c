ab9a23dcc25e63c9ad2524728072e36bcb749bec
Pedro Mesquita <pedro.mesquitamaia@gmail.com>
2023-12-03 20:18:37 -0300

    Trabalho terminado

--- INF1037/T04/main.c
+++ INF1037/T04/main.c
@@ -1,3 +1,9 @@
+/*
+    33D
+    Pedro Mesquita Maia
+    2312664
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -20,15 +26,63 @@ struct agenda
 typedef struct agenda Agenda;
 
 Agenda *criaAgenda(void);
-void insereOrd(Agenda *agenda, No *contato);
+int insereOrd(Agenda *agenda, No *contato);
 void listaLetra(Agenda *agenda, char chave_de_pesquisa);
+int retira(Agenda *agenda, char nome[]);
+void liberaCadastro(Agenda *agenda);
 
 int main(void)
 {
-    // Agenda *minhaAgenda = criaAgenda();
-    // listaLetra(minhaAgenda, 'b');
+    Agenda *minhaAgenda = criaAgenda();
 
-    // free(minhaAgenda);
+    // TESTE
+
+    No *contato1 = (No *)malloc(sizeof(No));
+    strcpy(contato1->nome, "Alice um");
+    strcpy(contato1->telefone, "123-456-7890");
+    contato1->prox = NULL;
+    insereOrd(minhaAgenda, contato1);
+
+    No *contato2 = (No *)malloc(sizeof(No));
+    strcpy(contato2->nome, "Bob");
+    strcpy(contato2->telefone, "987-654-3210");
+    contato2->prox = NULL;
+    insereOrd(minhaAgenda, contato2);
+
+    No *contato3 = (No *)malloc(sizeof(No));
+    strcpy(contato3->nome, "Charlie");
+    strcpy(contato3->telefone, "555-123-4567");
+    contato3->prox = NULL;
+    insereOrd(minhaAgenda, contato3);
+
+    No *contato4 = (No *)malloc(sizeof(No));
+    strcpy(contato4->nome, "Alice dois");
+    strcpy(contato4->telefone, "testando");
+    contato4->prox = NULL;
+    insereOrd(minhaAgenda, contato4);
+
+    No *contato5 = (No *)malloc(sizeof(No));
+    strcpy(contato5->nome, "Alice tres");
+    strcpy(contato5->telefone, "testando");
+    contato5->prox = NULL;
+    insereOrd(minhaAgenda, contato5);
+
+    No *contato6 = (No *)malloc(sizeof(No));
+    strcpy(contato6->nome, "Alice quatro");
+    strcpy(contato6->telefone, "testando");
+    contato6->prox = NULL;
+    insereOrd(minhaAgenda, contato6);
+
+    listaLetra(minhaAgenda, 'A');
+    listaLetra(minhaAgenda, 'B');
+    listaLetra(minhaAgenda, 'C');
+    
+    retira(minhaAgenda, "Alice um");
+    listaLetra(minhaAgenda, 'A');
+
+    // Liberando a memória alocada
+    liberaCadastro(minhaAgenda);
+
     return 0;
 }
 
@@ -37,7 +91,7 @@ Agenda *criaAgenda()
     Agenda *agenda = (Agenda *)malloc(sizeof(Agenda));
     if (agenda == NULL)
     {
-        fprintf(stderr, "Erro ao alocar memória\n");
+        exit(1);
     }
     for (int i = 0; i < 26; i++)
     {
@@ -46,7 +100,7 @@ Agenda *criaAgenda()
     return agenda;
 }
 
-void insereOrd(Agenda *agenda, No *contato)
+int insereOrd(Agenda *agenda, No *contato)
 {
     int pos;
     No *pAux;
@@ -62,12 +116,30 @@ void insereOrd(Agenda *agenda, No *contato)
     if (agenda->ponteiroLista[pos] == NULL)
     {
         agenda->ponteiroLista[pos] = contato;
-        return;
+        return 1;
     }
 
     pAux = agenda->ponteiroLista[pos];
-    contato->prox = pAux;
-    agenda->ponteiroLista[pos] = contato;
+    No *pAnterior = NULL;
+    while (pAux && strcmp(pAux->nome, contato->nome) < 0)
+    {
+        pAnterior = pAux;
+        pAux = pAux->prox;
+    }
+
+    if (pAnterior == NULL)
+    {
+        contato->prox = agenda->ponteiroLista[pos];
+        agenda->ponteiroLista[pos] = contato;
+        return 1;
+    }
+    else
+    {
+        contato->prox = pAnterior->prox;
+        pAnterior->prox = contato;
+        return 1;
+    }
+    return 0;
 }
 
 void listaLetra(Agenda *agenda, char chave_de_pesquisa)
@@ -84,10 +156,10 @@ void listaLetra(Agenda *agenda, char chave_de_pesquisa)
     printf("%c\n", chave_de_pesquisa);
     while (pNoAux)
     {
-        printf("%s: %s\n", agenda->ponteiroLista[pos]->nome, agenda->ponteiroLista[pos]->telefone);
-        pNoAux = agenda->ponteiroLista[pos]->prox;
+        printf("%s: %s\n", pNoAux->nome, pNoAux->telefone);
+        pNoAux = pNoAux->prox;
     }
-    printf("---------------");
+    printf("---------------\n");
 }
 
 int retira(Agenda *agenda, char nome[])
@@ -118,16 +190,5 @@ int retira(Agenda *agenda, char nome[])
 
 void liberaCadastro(Agenda *agenda)
 {
-    // for (int i = 0; i < 26; i++)
-    // {
-    //     No *pNo = agenda->ponteiroLista[i];
-    //     No *pAux = pNo;
-    //     while (pAux)
-    //     {
-    //         pAux = pNo->prox;
-    //         free(pNo);
-    //     }
-    // }
-
     free(agenda);
 }
