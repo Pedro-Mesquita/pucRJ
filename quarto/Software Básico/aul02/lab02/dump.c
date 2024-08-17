af6f238e3aab5aa242d24f4c0317ed29714dfc56
Pedro Mesquita <pedro.mesquitamaia@gmail.com>
2024-08-17 00:38:47 -0300

    adicionado as pastas e arquivos do quarto período

---
+++ quarto/Software Básico/aul02/lab02/dump.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void dump(void *p, int n);
+
+int main(void)
+{
+    int i = 10000;
+    dump(&i, sizeof(i));
+    return 0;
+}
+
+void dump(void *p, int n)
+{
+    unsigned char *p1 = p;
+    while (n--)
+    {
+        printf("%p - %02x\n", p1, *p1);
+        p1++;
+    }
+}
