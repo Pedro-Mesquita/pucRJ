32fd54b9cee8115e83a89a862bef0c30909a8cc9
Pedro Mesquita <pedro.mesquitamaia@gmail.com>
2024-09-10 14:29:43 -0300

    feito o lab06

--- quarto/Software Básico/aula06/lab05/main.c
+++ quarto/Software Básico/aula06/lab05/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 
 void dump(void *p, int n);
 
@@ -9,26 +10,101 @@ struct X
     int c;
 } x = {0xa1a2a3a4, 0xb1b2, 0xc1c2c3c4};
 
+struct X1
+{
+    char c1;
+    int i;
+    char c2;
+} x1 = {0xc1, 0x12121212, 0xc2};
+
+struct X2
+{
+    long l;
+    char c;
+} x2 = {0xa1a2a3a4a5a6a7a8, 0xc1};
+
+struct X3
+{
+    int i;
+    char c1;
+    char c2;
+} x3 = {0xa1a2a3a4, 0xc1, 0xc2};
+
+struct X4
+{
+    struct X2 x;
+    char c;
+} x4 = {{0xa1a2a3a4a5a6a7a8, 0xc1}, 0xc2};
+
+struct X5
+{
+    char c1;
+    char c2;
+    char c3;
+} x5 = {0xc1, 0xc2, 0xc3};
+
+struct X6
+{
+    short s1;
+    int i;
+    char c[3];
+    short s2;
+} x6 = {
+    0xc1c2,             // 2 bytes + 2 padding
+    0xc1c1c3c4,         // 4 bytes
+    {0xc1, 0xc2, 0xc3}, // 3 bytes
+    0xc1c2};            // 1 padding +  2 bytes + 2 padding
+
+union U1
+{
+    int i;
+    char c[5];
+} x7 = {.c = {0xc1, 0xc2, 0xc3, 0xc4, 0xc5}};
+
+union U2
+{
+    short s;
+    char c[5];
+} x8 = {.c = {0xc1, 0xc2, 0xc3, 0xc4, 0xc5}};
+
 int main(void)
 {
-    // int i, j;
-    // short a[2][3];
-    // int b[2];
 
-    // for (i = 0; i < 2; i++)
-    // {
-    //     b[i] = i;
-    //     for (j = 0; j < 3; j++)
-    //         a[i][j] = 3 * i + j;
-    // }
+    // printf("x:\n");
+    // dump(&x, sizeof(x));
+    // printf("tamanho de x %ld\n", sizeof(x));
 
-    // printf("b: \n");
-    // dump(b, sizeof(b));
-    // printf("a: \n");
-    // dump(a, sizeof(a));
+    // printf("X1:\n");
+    // dump(&x1, sizeof(x1));
+    // printf("tamanho de x1 %ld\n", sizeof(x1));
 
-    printf("x:\n");
-    dump(&x, sizeof(x));
+    // printf("X2:\n");
+    // dump(&x2, sizeof(x2));
+    // printf("tamanho de x2 %ld\n", sizeof(x2));
+
+    // printf("X3:\n");
+    // dump(&x3, sizeof(x3));
+    // printf("tamanho de x3 %ld\n", sizeof(x3));
+
+    // printf("X4:\n");
+    // dump(&x4, sizeof(x4));
+    // printf("tamanho de x4 %ld\n", sizeof(x4));
+
+    // printf("X5:\n");
+    // dump(&x5, sizeof(x5));
+    // printf("tamanho de x5 %ld\n", sizeof(x5));
+
+    // printf("X6:\n");
+    // dump(&x6, sizeof(x6));
+    // printf("tamanho de x6 %ld\n", sizeof(x6));
+
+    // printf("X7:\n");
+    // dump(&x7, sizeof(x7));
+    // printf("tamanho de x7 %ld\n", sizeof(x7));
+
+    printf("X8:\n");
+    dump(&x8, sizeof(x8));
+    printf("tamanho de x8 %ld\n", sizeof(x8));
 
     return 0;
 }
---
+++ quarto/Software Básico/aula08/lab06 (copy)/prog1.s
@@ -0,0 +1,57 @@
+/*
+int nums[] = {10, -21, -30, 45};
+int main() {
+  int i, *p;
+  for (i = 0, p = nums; i != 4; i++, p++)
+    printf("%d\n", *p);
+  return 0;
+}
+*/
+
+.data
+nums:  .int  10, -21, -30, 45
+Sf:  .string "%d\n"    # string de formato para printf
+
+.text
+.globl  main
+main:
+
+/********************************************************/
+/* mantenha este trecho aqui e nao mexa - prologo !!!   */
+  pushq   %rbp
+  movq    %rsp, %rbp
+  subq    $16, %rsp
+  movq    %rbx, -8(%rbp)
+  movq    %r12, -16(%rbp)
+/********************************************************/
+
+  movl  $0, %ebx  /* ebx = 0; */
+  movq  $nums, %r12  /* r12 = &nums */
+  movl $0, %eax /* eax = 0*/
+
+L1:
+  cmpl  $4, %ebx  /* if (ebx == 4) ? */
+  je  L2          /* goto L2 */
+
+  addl  $1, %ebx  /* ebx += 1; */
+  addl  (%r12), %eax  /* eax += rd12* pedro*/
+  addq  $4, %r12  /* r12 += 4; */
+  jmp  L1         /* goto L1; */
+
+L2:  
+
+/*************************************************************/
+/* este trecho imprime o valor de %eax (estraga %eax)  */
+  movq    $Sf, %rdi    /* primeiro parametro (ponteiro)*/
+  movl    %eax, %esi   /* segundo parametro  (inteiro)  pedro*/
+  call  printf       /* chama a funcao da biblioteca */
+/*************************************************************/
+
+/***************************************************************/
+/* mantenha este trecho aqui e nao mexa - finalizacao!!!!      */
+  movq  $0, %rax  /* rax = 0  (valor de retorno) */
+  movq  -8(%rbp), %rbx
+  movq  -16(%rbp), %r12
+  leave
+  ret      
+/***************************************************************/
